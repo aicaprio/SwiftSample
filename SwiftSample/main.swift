bfa2737c6e9361c739ee32dbaad173ccbc14ddd4
硕 <wangshuo@youbanban.com>
2020-09-03 13:56:24 +0800

    Initial Commit

---
+++ SwiftSample/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftSample
+//
+//  Created by Franklin on 2020/9/3.
+//  Copyright © 2020 AICAPRIO. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
